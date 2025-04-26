7814eb1e887c1a0d030d5d9c40705b9b9385b903
truc2004 <nguyenthanhtruc041204@gmail.com>
2025-04-26 14:49:58 +0700

    chua xong cap nhat, in, huy ve

--- src/main/java/Dao/NhaGaDAO.java
+++ src/main/java/Dao/NhaGaDAO.java
@@ -1,14 +1,28 @@
 package Dao;
 
 import Model.LichTrinh;
+import Model.NhaGa;
 import jakarta.persistence.EntityManager;
 
-public class NhaGaDAO extends GenericDAO<LichTrinh, String>{
-    public NhaGaDAO(Class<LichTrinh> clazz) {
+import java.util.List;
+
+public class NhaGaDAO extends GenericDAO<NhaGa, String>{
+    public NhaGaDAO(Class<NhaGa> clazz) {
         super(clazz);
     }
 
-    public NhaGaDAO(EntityManager em, Class<LichTrinh> clazz) {
+    public NhaGaDAO(EntityManager em, Class<NhaGa> clazz) {
         super(em, clazz);
     }
+
+    public NhaGa findBySTTNhaGa(int stt) {
+        List<NhaGa> nhaGaList = getAll();
+        for(NhaGa nhaGa : nhaGaList){
+            if(nhaGa.getSoThuTuNhaGa() == stt)
+                return nhaGa;
+        }
+
+        return null;
+    }
+
 }
--- src/main/java/Service/NhaGaService.java
+++ src/main/java/Service/NhaGaService.java
@@ -3,5 +3,5 @@ package Service;
 import Model.NhaGa;
 
 public interface NhaGaService extends GenericService<NhaGa, String>{
-
+    NhaGa findBySTTNhaGa(int stt);
 }
